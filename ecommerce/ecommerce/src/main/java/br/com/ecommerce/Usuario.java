d2e6c9e4865296f5f3c66ce85839383899aac65f
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-07-11 16:58:36 -0300

    usuario foi criado

---
+++ ecommerce/ecommerce/src/main/java/br/com/ecommerce/Usuario.java
@@ -0,0 +1,28 @@
+package br.com.ecommerce;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDateTime;
+
+@Entity
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    @Column(nullable = false, unique = true)
+    private String login;
+
+
+    @Column(nullable = false)
+    private String senha;
+
+    @PastOrPresent
+    private LocalDateTime criadoEm;
+
+
+
+
+}
