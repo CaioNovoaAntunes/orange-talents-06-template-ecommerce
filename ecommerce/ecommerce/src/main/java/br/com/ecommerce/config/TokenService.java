c488d5b18f79a7915d7c85c1fbf99be03ba0199c
CaioNovoaAntunes <caio_novoa_antunes@mail.com>
2021-07-13 22:25:34 -0300

    ajuste do nome do token

--- ecommerce/ecommerce/src/main/java/br/com/ecommerce/config/TokenService.java
+++ ecommerce/ecommerce/src/main/java/br/com/ecommerce/config/TokenService.java
@@ -26,7 +26,7 @@ public class TokenService {
         Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
 
         return Jwts.builder()
-                .setIssuer("Api do Mercado Livre")
+                .setIssuer(" Api do Mercado Livre ")
                 .setSubject(logado.getId().toString())
                 .setIssuedAt(hoje)
                 .setExpiration(dataExpiracao)
